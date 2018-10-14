c6deab13e56e696fe44a46546b5254f54d7c0150
Edward Louis Burgess <elburgess1@gmail.com>
2018-10-14 12:13:17 -0700

    gitignore and adding react router

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+const https = new XMLHttpRequest();
+const url = "https://dog.ceo/api/breeds/list/all"
+
+https.open("GET", url);
+https.send();
+
+https.onreadystatechange = () => {
+    console.log(typeof https.responseText, Object.keys(JSON.parse(https.responseText)["message"]));
+}
+
 const Index = () => {
     return <div>Hello React!</div>;
 };
